dabf48f7cd22c4a198e602b9011e3d47e0a8e70b
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-12 15:17:54 +0900

    spinnaker: add delete $APPENGINE_SERVICE_ACCOUNT_NAME to clean.bash

--- spinnaker/clean.bash
+++ spinnaker/clean.bash
@@ -5,6 +5,7 @@ set -ex
 
 gcloud iam --quiet service-accounts delete "$HALYARD_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com
 gcloud iam --quiet service-accounts delete "$GCS_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com
+gcloud iam --quiet service-accounts delete "$APPENGINE_SERVICE_ACCOUNT_NAME"@"$GCP_PROJECT".iam.gserviceaccount.com
 
 gcloud compute instances delete --zone="$HALYARD_ZONE" "$HALYARD_HOST"
 gcloud alpha container clusters delete --zone="$SPINNAKER_ZONE" "$SPINNAKER_CLUSTER"
