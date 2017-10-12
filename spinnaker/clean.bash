7925d643a2c66c44dc774bddce3bf72d8c6db293
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-12 18:23:54 +0900

    spinnaker: add '|| true' pipe for fail gcloud command

--- spinnaker/clean.bash
+++ spinnaker/clean.bash
@@ -3,9 +3,9 @@ set -ex
 
 . "$(dirname "$0")"/env.bash
 
-gcloud iam --quiet service-accounts delete "$HALYARD_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com
-gcloud iam --quiet service-accounts delete "$GCS_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com
-gcloud iam --quiet service-accounts delete "$APPENGINE_SERVICE_ACCOUNT_NAME"@"$GCP_PROJECT".iam.gserviceaccount.com
+gcloud iam --quiet service-accounts delete "$HALYARD_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com || true
+gcloud iam --quiet service-accounts delete "$GCS_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com || true
+gcloud iam --quiet service-accounts delete "$APPENGINE_SERVICE_ACCOUNT_NAME"@"$GCP_PROJECT".iam.gserviceaccount.com || true
 
-gcloud compute instances delete --zone="$HALYARD_ZONE" "$HALYARD_HOST"
-gcloud alpha container clusters delete --zone="$SPINNAKER_ZONE" "$SPINNAKER_CLUSTER"
+gcloud compute instances delete --zone="$HALYARD_ZONE" "$HALYARD_HOST" || true
+gcloud alpha container clusters delete --zone="$SPINNAKER_ZONE" "$SPINNAKER_CLUSTER" || true
--- spinnaker/credentials.bash
+++ spinnaker/credentials.bash
@@ -3,11 +3,11 @@ set -ex
 
 . "$(dirname "$0")"/env.bash
 
-gcloud iam service-accounts create "$HALYARD_SERVICE_ACCOUNT" --project="$GCP_PROJECT" --display-name "$HALYARD_SERVICE_ACCOUNT"
+gcloud iam service-accounts create "$HALYARD_SERVICE_ACCOUNT" --project="$GCP_PROJECT" --display-name "$HALYARD_SERVICE_ACCOUNT" || true
 HALYARD_SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list --project="$GCP_PROJECT" --filter="displayName:$HALYARD_SERVICE_ACCOUNT" --format='value(email)')
 gcloud projects add-iam-policy-binding "$GCP_PROJECT" --role roles/iam.serviceAccountKeyAdmin --member serviceAccount:"$HALYARD_SERVICE_ACCOUNT_EMAIL"
 gcloud projects add-iam-policy-binding "$GCP_PROJECT" --role roles/container.admin --member serviceAccount:"$HALYARD_SERVICE_ACCOUNT_EMAIL"
 
-gcloud iam service-accounts create "$GCS_SERVICE_ACCOUNT" --project="$GCP_PROJECT" --display-name "$GCS_SERVICE_ACCOUNT"
+gcloud iam service-accounts create "$GCS_SERVICE_ACCOUNT" --project="$GCP_PROJECT" --display-name "$GCS_SERVICE_ACCOUNT" || true
 GCS_SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list --project="$GCP_PROJECT" --filter="displayName:$GCS_SERVICE_ACCOUNT" --format='value(email)')
 gcloud projects add-iam-policy-binding "$GCP_PROJECT" --role roles/storage.admin --member serviceAccount:"$GCS_SERVICE_ACCOUNT_EMAIL"
