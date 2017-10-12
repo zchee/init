6ac1fe6a560e4ff4c9f739a75d658446062ad69c
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-12 18:26:47 +0900

    spinnaker: remove unnecessary flags and fix scpopes for create

--- spinnaker/create.bash
+++ spinnaker/create.bash
@@ -35,24 +35,22 @@ set -ex
 HALYARD_SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list --project="$GCP_PROJECT" --filter="displayName:$HALYARD_SERVICE_ACCOUNT" --format='value(email)')
 gcloud beta compute --project="$GCP_PROJECT" instances create "$HALYARD_HOST" \
   --zone="$HALYARD_ZONE" \
-  --machine-type="$HALYARD_MACHINE_TYPE" \
+  --scopes=cloud-platform \
   --service-account="$HALYARD_SERVICE_ACCOUNT_EMAIL" \
-  --scopes="https://www.googleapis.com/auth/devstorage.read_only,logging-write,monitoring-write,servicecontrol,service-management,https://www.googleapis.com/auth/trace.append" \
-  --min-cpu-platform="Automatic" \
-  --image-project="ubuntu-os-cloud" \
-  --image-family="ubuntu-1404-lts" \
+  --image-project=ubuntu-os-cloud \
+  --image-family=ubuntu-1404-lts \
+  --machine-type="$HALYARD_MACHINE_TYPE" \
   --boot-disk-size="10" \
   --boot-disk-type="pd-ssd"
 
 gcloud alpha container --project="$GCP_PROJECT" clusters create "$SPINNAKER_CLUSTER" \
   --zone="$SPINNAKER_ZONE" \
   --machine-type="$SPINNAKER_MACHINE_TYPE" \
-  --cluster-version="1.7.8"
   --num-nodes="$SPINNAKER_NUM_NODES" \
   --enable-cloud-logging \
   --no-enable-cloud-monitoring \
   --enable-legacy-authorization \
-  --scopes=default,compute-rw,https://www.googleapis.com/auth/devstorage.read_only,logging-write,monitoring,service-control,service-management,https://www.googleapis.com/auth/trace.append,userinfo-email
+  --scopes=default,bigquery,compute-rw,datastore,logging-write,monitoring,service-control,service-management,storage-ro,userinfo-email
 
 gcloud beta compute --project="$GCP_PROJECT" scp "$(dirname "$0")"/halyard/init.bash "$HALYARD_HOST":halyard.bash --zone="$HALYARD_ZONE"
 gcloud beta compute --project="$GCP_PROJECT" ssh "$HALYARD_HOST" --zone="$HALYARD_ZONE" --ssh-flag="-L 9000:localhost:9000" --ssh-flag="-L 8084:localhost:8084" --command="SPINNAKER_CLUSTER=$SPINNAKER_CLUSTER SPINNAKER_ZONE=$SPINNAKER_ZONE GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT SPINNAKER_VERSION=$SPINNAKER_VERSION ~/halyard.bash"
