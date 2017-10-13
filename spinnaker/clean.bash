29d9d84c6d107b63ab407918f52211efccfbefdd
Koichi Shiraishi <zchee.io@gmail.com>
2017-10-13 11:24:49 +0900

    spinnaker: add --quiet flag to delete {compute,container} command

--- spinnaker/clean.bash
+++ spinnaker/clean.bash
@@ -7,5 +7,5 @@ gcloud iam --quiet service-accounts delete "$HALYARD_SERVICE_ACCOUNT"@"$GCP_PROJ
 gcloud iam --quiet service-accounts delete "$GCS_SERVICE_ACCOUNT"@"$GCP_PROJECT".iam.gserviceaccount.com || true
 gcloud iam --quiet service-accounts delete "$APPENGINE_SERVICE_ACCOUNT_NAME"@"$GCP_PROJECT".iam.gserviceaccount.com || true
 
-gcloud compute instances delete --zone="$HALYARD_ZONE" "$HALYARD_HOST" || true
-gcloud alpha container clusters delete --zone="$SPINNAKER_ZONE" "$SPINNAKER_CLUSTER" || true
+gcloud compute --quiet instances delete --zone="$HALYARD_ZONE" "$HALYARD_HOST" || true
+gcloud alpha container --quiet clusters delete --zone="$SPINNAKER_ZONE" "$SPINNAKER_CLUSTER" || true
